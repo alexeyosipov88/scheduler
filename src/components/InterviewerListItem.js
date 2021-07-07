d1195190585dca1d695bd92d765b5c2f2421dd19
alexeyosipov88 <aoosipov88@gmail.com>
2021-07-07 03:32:25 -0400

    build interview list

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import classnames from 'classnames';
+import 'components/InterviewerListItem.scss'
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = classnames("interviewers__item",
+    { 'interviewers__item--selected': props.selected }
+  )
+  const interviewerImage = classnames("interviewers__item-image", 
+  {'interviewers__item-imgae--selected': props.selected })
+
+
+
+  return (
+    <li className={interviewerClass} >
+      <img
+        onClick={() => props.setInterviewer(props.id)}
+        className={interviewerImage}
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+}
