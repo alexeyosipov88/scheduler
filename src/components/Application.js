79dffd04594e431816ca35b36f4c6ac20e0dca5a
alexeyosipov88 <aoosipov88@gmail.com>
2022-06-15 15:39:03 -0400

    refactor application

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,35 +4,27 @@ import DayList from "components/DayList";
 
 import "components/Application.scss";
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors";
 // import getAppointmentsForDay from "../helpers/selectors";
 // import { getInterview } from "../helpers/selectors";
 // import { getInterviewersForDay } from "../helpers/selectors";
 // import useApplicationData from "helpers/hooks/useApplicationData";
 
 export default function Application(props) {
-  
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
   });
 
-  const daylyAppointments = [];
-
-  daylyAppointments.map((elem) => (
-    <Appointment key={elem.id} {...elem} />
-  ));
-  
+  const dailyAppointments = getAppointmentsForDay(state, state.day).map((elem) => <Appointment key={elem.id} {...elem} />);
   useEffect(() => {
-    
-    axios
-      .get("api/days")
-      .then((res) => {
-        setState((prev) => ({... prev, days: res.data}));
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    Promise.all([axios.get("/api/days"), axios.get("api/appointments")])
+    .then(
+      (res) => {
+        setState(prev => ({...prev, days: res[0].data, appointments: res[1].data}));
+      }
+    );
   }, []);
 
   return (
@@ -45,7 +37,11 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={state.days} value={state.day} setDay={(day) => setState({... state, day})} />
+          <DayList
+            days={state.days}
+            value={state.day}
+            setDay={(day) => setState({ ...state, day })}
+          />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -54,20 +50,16 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {daylyAppointments}
+        {dailyAppointments}
         <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
-
   // const { state, setDay, bookInterview, cancelInterview } =
   //   useApplicationData();
-
   // const dailyAppointments = getAppointmentsForDay(state, state.day);
   // const dailyInterviewers = getInterviewersForDay(state, state.day);
-
   //   return (
-
   //     <main className="layout">
   //       <section className="sidebar">
   //         <img
